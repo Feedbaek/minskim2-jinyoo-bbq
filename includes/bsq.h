8a708bf789a70f708124bc6a4a39031ae869feb5
Rob-Yoo <jin12243@naver.com>
2021-04-15 02:53:53 +0900

    r0b

--- includes/bsq.h
+++ includes/bsq.h
@@ -6,46 +6,45 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 15:26:33 by minskim2          #+#    #+#             */
-/*   Updated: 2021/04/15 02:06:11 by minskim2         ###   ########.fr       */
+/*   Updated: 2021/04/15 02:53:07 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BSQ_H
 # define BSQ_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
-void	ft_putchar(char c);
-void    ft_putstr(char *str);
-void    line_size(int fd, int *first, int *map_size);
-char    *read_line(int fd, int line_size);
+void		ft_putchar(char c);
+void		ft_putstr(char *str);
+void		line_size(int fd, int *first, int *map_size);
+char		*read_line(int fd, int line_size);
 int		ft_strlen(char *str);
 
 typedef struct	s_inform
 {
-	int		line;
-	int		size;
-	char	empty;
-	char	full;
-	char	object;
+	char empty;
+	char full;
+	char object;
+	int	line;
+	int	size;
 }		t_inform;
 
-void    set_inform(char *inform, t_inform *map);
-int     **search_sq(char **map, t_inform *inf);
+void		set_inform(char *inform, t_inform *map);
+int		**search_sq(char **map, t_inform *inf);
 
 typedef struct	s_box
 {
-	int	x;
-	int	y;
-	int	s_size;
+	int x;
+	int y;
+	int s_size;
 }		t_box;
 
-t_box   find_max_size(int **sq_size, int h, int w);
-void	convert_map(int **sq_size, t_inform *inf, char **map);
-void    print_map(char **map);
-void    bsq(int fd, t_inform *inf);
-//define
+t_box		find_max_size(int **sq_size, int h, int w);
+void		convert_map(int **sq_size, t_inform *inf, char **map);
+void		print_map(char **map);
+void		bsq(int fd, t_inform *inf);
 
 #endif
