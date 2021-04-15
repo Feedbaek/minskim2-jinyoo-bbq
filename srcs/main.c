a7d45a140b79b46dd57672383668345621b1848b
minskim2 <nicek789@naver.com>
2021-04-15 12:46:13 +0900

    a

--- srcs/main.c
+++ srcs/main.c
@@ -5,9 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/14 18:06:53 by minskim2          #+#    #+#             */
-/*   Updated: 2021/04/15 12:24:36 by jinyoo           ###   ########.fr       */
-/*   Updated: 2021/04/15 11:13:30 by minskim2         ###   ########.fr       */
+/*   Created: 2021/04/15 12:34:28 by minskim2          #+#    #+#             */
+/*   Updated: 2021/04/15 12:39:22 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +23,15 @@ int		bsq(int fd, t_inform *inf)
 	if (!map)
 		exit(1);
 	map[inf->line] = 0;
-	i = 0;
-	while (i < inf->line)
+	i = -1;
+	while (++i < inf->line)
 	{
-		j = 0;
+		j = -1;
 		map[i] = read_line(fd, inf->size);
-		while (map[i][j])
-		{
+		while (map[i][++j])
 			if (map[i][j] != inf->empty && map[i][j] != \
 					inf->object)
 				return (0);
-			j++;
-		}
-		i++;
 	}
 	result = search_sq(map, inf);
 	convert_map(result, inf, map);
@@ -57,19 +52,26 @@ void	set_bsq(int i, t_inform *inf, char *argv[])
 	close(fd);
 	fd = open(argv[i], O_RDONLY);
 	str = read_line(fd, first);
-	if(!set_inform(str, inf))
+	if (!set_inform(str, inf))
 	{
 		print_error();
 		return ;
 	}
 	inf->size = size;
-	if(!bsq(fd, inf))
+	if (!bsq(fd, inf))
 	{
 		print_error();
 		return ;
 	}
 	close(fd);
 	free(str);
+}
+
+void	ft_error(int *i, t_inform *inf)
+{
+	if (!inf)
+		exit(1);
+	*i = -1;
 }
 
 int		main(int argc, char *argv[])
@@ -80,12 +82,10 @@ int		main(int argc, char *argv[])
 	t_inform	*inf;
 
 	inf = (t_inform*)malloc(sizeof(t_inform));
-	if (!inf)
-		exit(1);
-	i = 1;
+	ft_error(&i, inf);
 	if (argc == 1)
 	{
-		fd = open("./Test", O_TRUNC | O_WRONLY|O_CREAT, 0644);
+		fd = open("./Test", O_TRUNC | O_WRONLY | O_CREAT, 0644);
 		while (read(0, &c, 1))
 			write(fd, &c, 1);
 		close(fd);
@@ -93,15 +93,12 @@ int		main(int argc, char *argv[])
 		set_bsq(1, inf, argv);
 	}
 	else
-	{
-		while (i < argc)
+		while (++i < argc)
 		{
 			set_bsq(i, inf, argv);
 			if (i < argc - 1)
 				ft_putchar('\n');
-			i++;
 		}
-	}
 	free(inf);
 	return (0);
 }
